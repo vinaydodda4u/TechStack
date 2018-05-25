c3ce860fa272773d3292f7bd4aa8090f45f324cc
git clone https://vdodda@bitbucket.org/clinivid/clinivid.server.git <vinay.dodda@healthcareinnovate.com.au>
2018-05-26 00:21:05 +0530

    Lambda Function Code Changes

--- IntelliBotsLambda/Function.cs
+++ IntelliBotsLambda/Function.cs
@@ -1,9 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+using System.Net.Http;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
 using Amazon.Lambda.Core;
+
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -12,16 +12,49 @@ namespace IntelliBotsLambda
 {
     public class Function
     {
-        
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+
+        public const string INVOCATION_NAME = "Country Info";
+
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
         {
-            return input?.ToUpper();
+
+            var requestType = input.GetRequestType();
+            if (requestType == typeof(IntentRequest))
+            {
+                return MakeSkillResponse(
+                        $"Hello Infotec! This is the first response from your Alexa skill using c sharp.",
+                        true);
+            }
+            else
+            {
+                return MakeSkillResponse(
+                        $"I don't know how to handle this intent. Please say something like Alexa, ask {INVOCATION_NAME} about Canada.",
+                        true);
+            }
+        }
+
+
+        private SkillResponse MakeSkillResponse(string outputSpeech,
+            bool shouldEndSession,
+            string repromptText = "Just say, tell me about Canada to learn more. To exit, say, exit.")
+        {
+            var response = new ResponseBody
+            {
+                ShouldEndSession = shouldEndSession,
+                OutputSpeech = new PlainTextOutputSpeech { Text = outputSpeech }
+            };
+
+            if (repromptText != null)
+            {
+                response.Reprompt = new Reprompt() { OutputSpeech = new PlainTextOutputSpeech() { Text = repromptText } };
+            }
+
+            var skillResponse = new SkillResponse
+            {
+                Response = response,
+                Version = "1.0"
+            };
+            return skillResponse;
         }
     }
 }
